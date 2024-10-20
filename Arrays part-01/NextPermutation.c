bba7933dbcac31d52286129565c0a8dec4ed0d54
EternalRavager <abhishek.sksuraj@gmail.com>
2024-10-20 19:58:21 +0530

    Next Permutation

--- Arrays part-01/NextPermutation.c
+++ Arrays part-01/NextPermutation.c
@@ -1,3 +1,29 @@
-//
-// Created by Abhishek on 20-10-2024.
-//
+void reverse(int* nums, int l, int r) {
+    while (l < r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+        l++;
+        r--;
+    }
+}
+
+void nextPermutation(int* nums, int numsSize) {
+    int i;
+    for (i = numsSize - 2; i >= 0; --i) {
+        if (nums[i] < nums[i + 1]) {
+            break;
+        }
+    }
+    if (i >= 0) {
+        for (int j = numsSize - 1; j > i; --j) {
+            if (nums[j] > nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                break;
+            }
+        }
+    }
+    reverse(nums, i + 1, numsSize - 1);
+}
