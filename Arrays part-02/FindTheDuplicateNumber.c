fd5ecf6a9ad2b3e8ebb893af894f5064e76ac6ce
EternalRavager <abhishek.sksuraj@gmail.com>
2024-11-01 21:43:41 +0530

    Find Duplicate

---
+++ Arrays part-02/FindTheDuplicateNumber.c
@@ -0,0 +1,12 @@
+int findDuplicate(int* nums, int numsSize) {
+    int* seen = (int*)calloc(numsSize, sizeof(int));
+    for (int i = 0; i < numsSize; i++) {
+        if (seen[nums[i]] == 1) {
+            free(seen);
+            return nums[i];
+        }
+        seen[nums[i]] = 1;
+    }
+    free(seen);
+    return -1;
+}
